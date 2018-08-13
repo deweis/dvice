0473564ec89f7102d28ad3969c18895f100d2e3c
Dennis <deweis@gmx.ch>
2018-08-13 12:38:26 +0200

    Add some more quotes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quotes = [
+const quotes = [// Thank you freeCodeCamp.org for most of the quotes in here!
   { author: 'Jonathan Swift',
     quote: 'May you live every day of your life', },
   { author: 'George Bernard Shaw',
@@ -9,6 +9,24 @@ const quotes = [
     quote: 'We are what we believe we are.', },
   { author: 'Thomas A. Edison',
     quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.', },
+  { author: 'Franklin D. Roosevelt',
+    quote: 'The only limit to our realization of tomorrow, will be our doubts of today.', },
+  { author: 'Confucius',
+    quote: 'We have two lives, and the second begins when we realize we only have one.', },
+  { author: 'Mario Quintana',
+    quote: 'Don\'t waste your time chasing butterflies. Mend your garden, and the butterflies will come.', },
+  { author: 'Edith Wharton',
+    quote: 'There are two ways of spreading light: to be the candle or the mirror that reflects it.', },
+  { author: 'Charles Darwin',
+    quote: 'It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.', },
+  { author: 'Leonardo da Vinci',
+    quote: 'Learning never exhausts the mind.', },
+  { author: 'Rob Siltanen',
+    quote: 'People who are crazy enough to think they can change the world, are the ones who actually do.', },
+  { author: 'Ghandi',
+    quote: 'Be the change you wish to see in the world.', },
+  { author: 'Jacque Fresco',
+    quote: 'If you think we can\'t change the world, it just means you\'re not one of those that will.', },
 ];
 
 /**
